c39128a878f976f57a472a94728e68a033b86eeb
fengwenwei <feng.feng@biposervice.com>
2023-07-07 13:33:29 +0800

    first commit

---
+++ src/test/java/com/learn/simple_demo/SimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.simple_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
