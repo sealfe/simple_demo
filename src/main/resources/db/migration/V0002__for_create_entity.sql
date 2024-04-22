aca5cf4736da609f2410f3b2e92a81fc09fae9d4
fengwenwei <feng.feng@biposervice.com>
2024-04-22 11:25:20 +0800

    learn seal

--- src/main/resources/db/migration/V0002__for_create_entity.sql
+++ src/main/resources/db/migration/V0002__for_create_entity.sql
@@ -2,12 +2,12 @@ create table entity
 (
     id              varchar(255) not null
         primary key,
-    name            varchar(255) not null,
-    creator_id      varchar(255) not null,
-    updater_id      varchar(255) not null,
-    created_at      timestamp    not null,
-    created_at_zone varchar(255) not null,
-    updated_at      timestamp    not null,
-    updated_at_zone varchar(255) not null,
-    is_deleted      boolean      not null
+    name            varchar(255) null,
+    creator_id      varchar(255) null,
+    updater_id      varchar(255) null,
+    created_at      timestamp    null,
+    created_at_zone varchar(255) null,
+    updated_at      timestamp    null,
+    updated_at_zone varchar(255) null,
+    is_deleted      boolean      null
 );
